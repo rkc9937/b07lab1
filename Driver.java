df3f1309991c586ed1e31de79b074f1fdfb0130d
Rishabh Chowdhary <rishabh.chowdhary177@gmail.com>
2023-06-11 12:59:16 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -2,28 +2,104 @@ import java.io.*;
 import java.util.*;
 public class Driver {
     public static void main(String [] args) {
-        double [] coefficents = {1.0, 2.0, 3.0};
-        int [] exponents = {1, 2, 3};
-        Polynomial p = new Polynomial(coefficents, exponents);
-        System.out.println(p.evaluate(2.0));
-        //Try add method
-        double [] coefficents2 = {1.0, 2.0, 3.0};
-        int [] exponents2 = {1, 2, 3};
-        Polynomial p2 = new Polynomial(coefficents2, exponents2);
-        p2.add(p);
-        System.out.println(p2.evaluate(2.0));
-        //Try multiply method
-        double [] coefficents3 = {1.0, 2.0, 3.0};
-        int [] exponents3 = {1, 2, 3};
-        Polynomial p3 = new Polynomial(coefficents3, exponents3);
-        p3.multiply(p);
-        for (double i: p3.nonzerocoefficents){
-            System.out.println("Coefficents of p3 after multiplication " + i);
-        }
-        for(int i: p3.exponents){
-            System.out.println("Exponents of p3 after multiplication " + i);
-        }
-        //Try saveToFile method
-        p3.saveToFile("test.txt");
+        //Test no argument constructor
+        Polynomial p = new Polynomial();
+        System.out.println("No argument constructor: " + Arrays.toString(p.nonzerocoefficents) + " " + Arrays.toString(p.exponents));
+        //Test argument constructor
+        double [] nonzerocoeffs = {1.0, 2.0, 3.0};
+        int [] expon = {1, 2, 3};
+        Polynomial p2 = new Polynomial(nonzerocoeffs, expon);
+        System.out.println("Argument constructor: " + Arrays.toString(p2.nonzerocoefficents) + " " + Arrays.toString(p2.exponents));
+        //Test file constructor
+        File f = new File("poly.txt");
+        Polynomial p3 = new Polynomial(f);
+        System.out.println("File constructor: " + Arrays.toString(p3.nonzerocoefficents) + " " + Arrays.toString(p3.exponents));
+
+        //Test add method
+        Polynomial p4 = p2.add(p3);
+        System.out.println("Add method: " + Arrays.toString(p4.nonzerocoefficents) + " " + Arrays.toString(p4.exponents));
+
+        //Test evaluate method
+        System.out.println("Evaluate method: " + p4.evaluate(2.0));
+
+        //Test hasRoot method
+        System.out.println("Has root method: " + p4.hasRoot(2.0));
+
+        //Test multiply method
+        Polynomial p5 = p2.multiply(p3);
+        System.out.println("Multiply method: " + Arrays.toString(p5.nonzerocoefficents) + " " + Arrays.toString(p5.exponents));
+
+        //Test saveToFile method
+        p5.saveToFile("test.txt");
+
+        System.out.println("Testing zero polynomaial");
+        double [] zeroCoeffs = {0.0};
+        int [] zeroExponents = {0};
+        Polynomial p6 = new Polynomial(zeroCoeffs, zeroExponents);
+        System.out.println("Zero polynomial: " + Arrays.toString(p6.nonzerocoefficents) + " " + Arrays.toString(p6.exponents));
+        Polynomial p7 = p6.add(p6);
+        System.out.println("Zero polynomial add: " + Arrays.toString(p7.nonzerocoefficents) + " " + Arrays.toString(p7.exponents));
+        Polynomial p8 = p6.multiply(p6);
+        System.out.println("Zero polynomial multiply: " + Arrays.toString(p8.nonzerocoefficents) + " " + Arrays.toString(p8.exponents));
+        System.out.println("Zero polynomial evaluate: " + p6.evaluate(2.0));
+        System.out.println("Zero polynomial hasRoot: " + p6.hasRoot(2.0));
+        //Test multiply zero polynomial
+        Polynomial p9 = p2.multiply(p6);
+        System.out.println("Multiply zero polynomial: " + Arrays.toString(p9.nonzerocoefficents) + " " + Arrays.toString(p9.exponents));
+        
+
+        //Print out the polynomial's non-zero coefficients and exponents
+        System.out.println("Polynomial: " + Arrays.toString(p.nonzerocoefficents) + " " + Arrays.toString(p.exponents));
+
+        double [] x  = {3.0,2.0,7.0};
+        int [] x_expon = {0,1,2};
+
+        double [] q = {4,9,4,8};
+        int [] q_expon = {0,1,3,2};
+        Polynomial poly_for_multiply1 = new Polynomial(x, x_expon);
+        Polynomial poly_for_multiply2 = new Polynomial(q, q_expon);
+
+        Polynomial multiplied = poly_for_multiply1.multiply(poly_for_multiply2);
+
+        System.out.println("Multiply: " + Arrays.toString(poly_for_multiply1.multiply(poly_for_multiply2).nonzerocoefficents) + " " + Arrays.toString(poly_for_multiply1.multiply(poly_for_multiply2).exponents));
+
+        //Save to file
+        multiplied.saveToFile("test.txt");
+
+        double [] h = {1.0};
+        int [] h_expon = {1};
+        double [] h1 = {1.0, 2};
+        int [] h1_expon = {2, 0};
+        Polynomial multiply_test_2 = new Polynomial(h ,h_expon);
+        Polynomial multiply_test_3 = new Polynomial(h1, h1_expon);
+
+        Polynomial multiplied2 = multiply_test_2.multiply(multiply_test_3);
+        System.out.println("Multiply: " + Arrays.toString(multiplied2.nonzerocoefficents) + " " + Arrays.toString(multiplied2.exponents));
+
+        //Testing x^2+2x * 2x+1
+
+        double [] x2 = {1.0,2.0};
+        int [] x2_expon = {2,1};
+        double [] x3 = {2.0, 1.0};
+        int [] x3_expon = {1, 0};
+
+        Polynomial multiply_test_4 = new Polynomial(x2, x2_expon);
+        Polynomial multiply_test_5 = new Polynomial(x3, x3_expon);
+
+        Polynomial multiplied3 = multiply_test_4.multiply(multiply_test_5);
+        System.out.println("Multiply: " + Arrays.toString(multiplied3.nonzerocoefficents) + " " + Arrays.toString(multiplied3.exponents));
+
+   
+        Polynomial zerPolynomial = new Polynomial(zeroCoeffs, zeroExponents);
+        //Zero polynomial multiplied by another polynomial
+        Polynomial zeroMultiplied = multiplied3.multiply(zerPolynomial);
+        System.out.println("Zero multiplied: " + Arrays.toString(zeroMultiplied.nonzerocoefficents) + " " + Arrays.toString(zeroMultiplied.exponents));
+
+        //Zero polynomial added to another polynomial
+        Polynomial zeroAdded = multiplied3.add(zerPolynomial);
+        System.out.println("Zero added: " + Arrays.toString(zeroAdded.nonzerocoefficents) + " " + Arrays.toString(zeroAdded.exponents));
+
+
+
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -6,10 +6,9 @@ public class Polynomial{
 
     //No arguement constructor
     public Polynomial(){
-        double [] zero = {0.0};
-        int [] zero2 = {0};
-        this.nonzerocoefficents = zero;
-        this.exponents = zero2;
+        //Changed due to piazza post listing
+        this.nonzerocoefficents = null;
+        this.exponents = null;
 
     }
 
@@ -20,36 +19,89 @@ public class Polynomial{
     }
 
     //File constructor
-    public Polynomial(File f, double [] nonzerocoefficents, int [] exponents){
+    public Polynomial(File f){
         try {
+            //Polynomial will be formatted like this: 5x-3x2+2x3
             Scanner sc = new Scanner(f);
-            String line = sc.nextLine();
-            String [] split = line.split("\\+");
+            String s = sc.nextLine();
+            //Consider the fact that the polynomial may have a negative coefficient
+            s = s.replaceAll("-", "+-");
+            String [] split = s.split("\\+");
             double [] nonzerocoeffs = new double[split.length];
             int [] expon = new int[split.length];
             for(int i = 0; i < split.length; i++){
-                String [] split2 = split[i].split("x");
-                nonzerocoeffs[i] = Double.parseDouble(split2[0]);
-                expon[i] = Integer.parseInt(split2[1].substring(1));
+                if(split[i].contains("x")){
+                    String [] split2 = split[i].split("x");
+                    nonzerocoeffs[i] = Double.parseDouble(split2[0]);
+                    expon[i] = Integer.parseInt(split2[1]);
+                }
+                else{
+                    nonzerocoeffs[i] = Double.parseDouble(split[i]);
+                    expon[i] = 0;
+                }
+                
             }
+            sc.close();
             this.nonzerocoefficents = nonzerocoeffs;
-            this.exponents = expon;
+            this.exponents = expon; 
         }
         catch (FileNotFoundException e){
             System.out.println("File not found");
         }
     }
 
-    //add method
+    //add method, recheck
+    //Recall redundant exponents are allowed
+    //We must remove all 0 entries
     public Polynomial add(Polynomial p){
-        for(int i = 0; i < nonzerocoefficents.length; i++){
-            p.nonzerocoefficents[i] += nonzerocoefficents[i];
+        //We are allowed to have redundant exponents, so we can just add the arrays
+        //Check if either array is null or empty
+        double [] zeroCoeffs = {0.0};
+        if(this.nonzerocoefficents == null || p.nonzerocoefficents == null){
+            return null;
         }
-        return p;
+        if(this.nonzerocoefficents.length == 0){
+            //Empty array means 0 polynomial
+            this.nonzerocoefficents = zeroCoeffs;
+        }
+        if(p.nonzerocoefficents.length == 0){
+            p.nonzerocoefficents = zeroCoeffs;
+        }
+        for(int i = 0; i < this.nonzerocoefficents.length; i++){
+            //i = 0, 5, 0 non-zero and expon respectively
+            //We want to traverse p's non-zero exponent's to see if there's a 0
+            //If there is a 0, we want to add the coefficent to the coefficent of the same exponent
+            //If there is no 0, we want to add the coefficent and exponent to the end of the array, but do this after we search through app of our exponents
+            for(int j = 0; j < p.nonzerocoefficents.length; j++){
+                if(this.exponents[i] == p.exponents[j]){
+                    this.nonzerocoefficents[i] += p.nonzerocoefficents[j];
+                }
+            }
+        }
+        //Deal with removing zeros
+        int numzero = 0;
+        for(int i = 0; i < this.nonzerocoefficents.length; i++){
+            if(this.nonzerocoefficents[i] == 0){
+                numzero++;
+            }
+        }
+        double [] newcoeffs = new double[this.nonzerocoefficents.length - numzero];
+        int count = 0;
+        for(int i = 0; i < this.nonzerocoefficents.length; i++){
+            if(this.nonzerocoefficents[i] != 0){
+                newcoeffs[count] = this.nonzerocoefficents[i];
+                count++;
+            }
+        }
+        this.nonzerocoefficents = newcoeffs;
+        return this;
     }
 
     //evaluate method
     public double evaluate(double x){
+        if(nonzerocoefficents == null || nonzerocoefficents.length == 0){
+            return 0;
+        }
         double sum = 0;
         for(int i = 0; i < nonzerocoefficents.length; i++){
             sum += nonzerocoefficents[i] * Math.pow(x, exponents[i]);
@@ -59,6 +111,9 @@ public class Polynomial{
 
     //hasRoot method
     public boolean hasRoot(double x){
+        if(nonzerocoefficents == null || nonzerocoefficents.length == 0){
+            return false;
+        }
         if(evaluate(x) == 0){
             return true;
         }
@@ -66,31 +121,125 @@ public class Polynomial{
     }
 
     public Polynomial multiply(Polynomial p){
-        double [] newCoefficents = new double[nonzerocoefficents.length * p.nonzerocoefficents.length];
-        int [] newExponents = new int[exponents.length * p.exponents.length];
+        //For each coefficent in this, we want to multiply it by each coefficent in p, and then add the exponents
+        //New array for coefficents and exponents
+        int size1 = nonzerocoefficents.length;
+        int size2 = p.nonzerocoefficents.length;
+        double [] zeroCoeffs = {0.0};
+        double [] empty = {};
+        if(size1==0){
+            this.nonzerocoefficents = zeroCoeffs;
+        }
+        if(size2==0){
+            p.nonzerocoefficents = zeroCoeffs;
+        }
+        if(size1==0 && size2 == 0){
+            int [] zeroExponents = {0};
+            return new Polynomial(empty, zeroExponents);
+        }
+        double[] resultCoefficients = new double[size1 + size2 - 1];
+        int[] resultExponents = new int[size1 + size2 - 1];
+
+        //Exception of 0 polynomial and 1 polynomial
+        if (size1 == 0 || size2 == 0) {
+            return new Polynomial(resultCoefficients, resultExponents);
+        }
+        if (size1 == 1 && size2 == 1) {
+            resultCoefficients[0] = nonzerocoefficents[0] * p.nonzerocoefficents[0];
+            resultExponents[0] = exponents[0] + p.exponents[0];
+            //Remove 0 coefficents
+            if (resultCoefficients[0] == 0) {
+                return new Polynomial(resultCoefficients, resultExponents);
+            }
+            return new Polynomial(resultCoefficients, resultExponents);
+        }
+        //If just one polynomial is of size 1
+        if (size1 == 1) {
+            for (int i = 0; i < size2; i++) {
+                resultCoefficients[i] = nonzerocoefficents[0] * p.nonzerocoefficents[i];
+                resultExponents[i] = exponents[0] + p.exponents[i];
+            }
+            int numzero = 0;
+            for(int i = 0; i < resultCoefficients.length; i++){
+                if(resultCoefficients[i] == 0){
+                    numzero++;
+                }
+            }
+            double [] newcoeffs = new double[resultCoefficients.length - numzero];
+            int count = 0;
+            for(int i = 0; i < resultCoefficients.length; i++){
+                if(resultCoefficients[i] != 0){
+                    newcoeffs[count] = resultCoefficients[i];
+                    count++;
+                }
+            }
+            return new Polynomial(newcoeffs, resultExponents);
+        }
+        else if(size2 == 1){
+            for (int i = 0; i < size1; i++) {
+                resultCoefficients[i] = nonzerocoefficents[i] * p.nonzerocoefficents[0];
+                resultExponents[i] = exponents[i] + p.exponents[0];
+            }
+            //Remove 0 coefficents from resultCoefficents
+            int numzero = 0;
+            for(int i = 0; i < resultCoefficients.length; i++){
+                if(resultCoefficients[i] == 0){
+                    numzero++;
+                }
+            }
+            double [] newcoeffs = new double[resultCoefficients.length - numzero];
+            int count = 0;
+            for(int i = 0; i < resultCoefficients.length; i++){
+                if(resultCoefficients[i] != 0){
+                    newcoeffs[count] = resultCoefficients[i];
+                    count++;
+                }
+            }
+            return new Polynomial(newcoeffs, resultExponents);
+        }
+        //If both polynomials are of size greater than 1
+        for (int i = 0; i < size1; i++) {
+            for (int j = 0; j < size2; j++){
+                resultCoefficients[i + j] += nonzerocoefficents[i] * p.nonzerocoefficents[j];
+                resultExponents[i + j] = exponents[i] + p.exponents[j];
+            }
+        }
+
+        //Remove zero coefficents
+        int numzero = 0;
+        for(int i = 0; i < resultCoefficients.length; i++){
+            if(resultCoefficients[i] == 0){
+                numzero++;
+            }
+        }
+        double [] newcoeffs = new double[resultCoefficients.length - numzero];
+        int [] newexpon = new int[resultExponents.length - numzero];
         int count = 0;
-        for(int i = 0; i < nonzerocoefficents.length; i++){
-            for(int j = 0; j < p.nonzerocoefficents.length; j++){
-                newCoefficents[count] = nonzerocoefficents[i] * p.nonzerocoefficents[j];
-                newExponents[count] = exponents[i] + p.exponents[j];
+        for(int i = 0; i < resultCoefficients.length; i++){
+            if(resultCoefficients[i] != 0){
+                newcoeffs[count] = resultCoefficients[i];
+                newexpon[count] = resultExponents[i];
                 count++;
             }
         }
-        return new Polynomial(newCoefficents, newExponents);
+        return new Polynomial(newcoeffs, newexpon);
     }
 
     public void saveToFile(String fileName){
         try{
-            PrintStream ps = new PrintStream(fileName);
+            PrintWriter pw = new PrintWriter(fileName);
             for(int i = 0; i < nonzerocoefficents.length; i++){
-                if(i == nonzerocoefficents.length - 1){
-                    //Do we need int or double?
-                    ps.print(nonzerocoefficents[i] + "x" + exponents[i]);
+                if(exponents[i] == 0){
+                    pw.print(nonzerocoefficents[i] + "+");
+                }
+                else if(i == nonzerocoefficents.length - 1){
+                    pw.print(nonzerocoefficents[i] + "x" + exponents[i]);
                 }
                 else{
-                    ps.print(nonzerocoefficents[i] + "x" + exponents[i] + "+");
+                    pw.print(nonzerocoefficents[i] + "x" + exponents[i] + "+");
                 }
             }
+            pw.close();
         }
         catch(FileNotFoundException e){
             System.out.println("File not found");
